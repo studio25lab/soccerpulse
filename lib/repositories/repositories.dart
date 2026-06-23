57d10467201e69efb55d2da54183fb1d2351be93
Francesco <fradg15@gmail.com>
2026-06-23 16:23:35 +0200

    chore: rimuove 42 file orfani (~18225 righe codice morto)

--- lib/repositories/repositories.dart
+++
@@ -1,9 +0,0 @@
-// lib/repositories/repositories.dart
-// Barrel export per tutti i repository
-
-export 'match_repository.dart';
-export 'mock_match_repository.dart';
-export 'api_match_repository.dart';
-export 'player_repository.dart';
-export 'mock_player_repository.dart';
-export 'repository_provider.dart';
